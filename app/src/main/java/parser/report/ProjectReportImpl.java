9b6cf38b38f3b65e191380ff75e140531eeca0a4
filippo.barbari <filippo.barbari@studio.unibo.it>
2022-04-28 14:27:27 +0200

    Added ProjectReportImpl

---
+++ app/src/main/java/parser/report/ProjectReportImpl.java
@@ -0,0 +1,20 @@
+package parser.report;
+
+import java.util.List;
+
+public class ProjectReportImpl implements ProjectReport {
+	@Override
+	public ClassReport getMainClass() {
+		return null;
+	}
+
+	@Override
+	public List<ClassReport> getAllClasses() {
+		return null;
+	}
+
+	@Override
+	public ClassReport getClassReport(String fullClassName) {
+		return null;
+	}
+}
