958acfa099044a4ea5f2468a5ed288c1268b2090
Ledmington <ledmington.dev@gmail.com>
2022-05-02 13:02:17 +0200

    Reorganized gradle project structure

--- nbodies/build.gradle.kts
+++ nbodies/build.gradle.kts
@@ -17,17 +17,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 
     implementation("com.google.guava:guava:30.1.1-jre")
-
-    implementation("io.vertx:vertx-core:4.2.6")
-    implementation("io.vertx:vertx-web:4.2.6")
-    implementation("io.vertx:vertx-web-client:4.2.6")
-
-    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.24.2")
 }
 
 application {
-    //mainClass.set("nbodies.NBodies")
-    mainClass.set("parser.AsyncParser")
+    mainClass.set("nbodies.NBodies")
 }
 
 tasks.test {
