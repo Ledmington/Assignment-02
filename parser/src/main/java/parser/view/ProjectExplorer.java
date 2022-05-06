44821cd57497996f8d49b0e06a5a89440a81f53b
Ledmington <ledmington.dev@gmail.com>
2022-05-06 10:18:41 +0200

    Added packages and subpackages into ProjectExplorer

--- parser/src/main/java/parser/view/ProjectExplorer.java
+++ parser/src/main/java/parser/view/ProjectExplorer.java
@@ -6,6 +6,8 @@ import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.BorderLayout;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.vertx.core.eventbus.EventBus;
 import parser.ProjectElement;
@@ -14,22 +16,55 @@ public class ProjectExplorer extends JPanel {
     
     private final JTree tree;
     private final DefaultMutableTreeNode rootNode;
+    private final Map<String, DefaultMutableTreeNode> nodes = new HashMap<>();
+    private final Map<String, String> parents = new HashMap<>();
 
     public ProjectExplorer(final EventBus bus) {
         super();
 
         this.setLayout(new BorderLayout());
 
-        rootNode = new DefaultMutableTreeNode("Root");
+        rootNode = new DefaultMutableTreeNode("root");
+        nodes.put("root", rootNode);
         tree = new JTree(rootNode);
         this.add(new JScrollPane(tree), BorderLayout.CENTER);
 
+        bus.consumer(ProjectElement.PACKAGE.getName(), handler -> {
+            if (handler.body() == null) return;
+            final String fullPackageName = (String) handler.body();
+            final String[] packagePath = fullPackageName.split("\\.");
+            if (packagePath.length == 1) {
+                // direct son of root
+                if (!nodes.containsKey(fullPackageName)) {
+                    parents.put(fullPackageName, "root");
+                    final DefaultMutableTreeNode packageNode = new DefaultMutableTreeNode(fullPackageName);
+                    nodes.put(fullPackageName, packageNode);
+                    SwingUtilities.invokeLater(() -> nodes.get("root").add(packageNode));
+                }
+            } else {
+                // indirect son of root
+                // adding all parents before the son
+                for (int i=0; i<packagePath.length-1; i++) {
+                    final String packageName = packagePath[i+1];
+                    if (!nodes.containsKey(packageName)) {
+                        final String parentPackage = packagePath[i];
+                        parents.put(packageName, parentPackage);
+                        final DefaultMutableTreeNode packageNode = new DefaultMutableTreeNode(packageName);
+                        nodes.put(packageName, packageNode);
+                        SwingUtilities.invokeLater(() -> nodes.get(parentPackage).add(packageNode));
+                    }
+                }
+            }
+        });
+        /*
         for (ProjectElement element : ProjectElement.values()) {
             bus.consumer(element.getName(), handler -> {
                 final String nodeName = (String) handler.body();
                 System.out.println("Found " + element.getName() + ": " + nodeName);
-                SwingUtilities.invokeLater(() -> rootNode.add(new DefaultMutableTreeNode(nodeName)));
+                final DefaultMutableTreeNode node = new DefaultMutableTreeNode(nodeName);
+                nodes.put(nodeName, node);
+                SwingUtilities.invokeLater(() -> rootNode.add(node));
             });
-        }
+        }*/
     }
 }
