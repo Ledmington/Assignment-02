1dc189479bd7b8bbf19bc3f5092cdb94cf5f7e5b
Ledmington <ledmington.dev@gmail.com>
2022-05-08 21:39:44 +0200

    Changed rxjava version to 3.1.4

--- reactive/build.gradle.kts
+++ reactive/build.gradle.kts
@@ -24,11 +24,11 @@ dependencies {
 
     implementation("com.google.guava:guava:30.1.1-jre")
 
-    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.4")
 }
 
 application {
-    mainClass.set("reactive.AsyncParser")
+    mainClass.set("reactive.ReactiveParser")
 }
 
 tasks.test {
