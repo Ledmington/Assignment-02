3ba154e3a74cec8b90a675dca2f9f1b0795d1e7b
Ledmington <ledmington.dev@gmail.com>
2022-05-09 19:37:35 +0200

    boh

--- reactive/src/main/java/reactive/AsyncParser.java
+++ reactive/src/main/java/reactive/AsyncParser.java
@@ -5,6 +5,6 @@ import reactive.view.ParserFrame;
 public class AsyncParser {
     public static void main(final String[] args) {
         ProjectAnalyzer pa = new ProjectAnalyzerImpl();
-        //new ParserFrame(pa);
+        new ParserFrame();
     }
 }
