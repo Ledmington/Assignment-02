234e301cfd71b13d15b38c18eef55224ef5f3ae1
Filippo Benvenuti <filippo.benvenuti3@studio.unibo.it>
2022-05-10 09:22:58 +0200

    Addded java parser

--- reactive/build.gradle.kts
+++ reactive/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation("com.google.guava:guava:30.1.1-jre")
 
     implementation("io.reactivex.rxjava3:rxjava:3.1.4")
+
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.24.2")
 }
 
 application {
